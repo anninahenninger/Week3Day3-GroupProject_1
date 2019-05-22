8f62351c92790a9b360917775b16b9a22d1a38b5
judith <judithmarialang@gmail.com>
2019-05-22 11:05:48 +0200

    adds schedule constructor and 2d array

--- src/GroupProject1_Judith_Annina_Christian/Schedule.java
+++ src/GroupProject1_Judith_Annina_Christian/Schedule.java
@@ -1,4 +1,34 @@
 package GroupProject1_Judith_Annina_Christian;
 
+import java.util.ArrayList;
+
 public class Schedule {
+
+
+    private String weekDay;
+    private String [][] subjHour; //subject/hour
+
+
+
+    public Schedule(String weekDay, String [][] subjHour){
+
+        this.weekDay = weekDay;
+        this.subjHour= subjHour;
+
+
+    }
+
+    public void getWeekDay(){
+        this.weekDay = weekDay;
+    }
+
+    public void setWeekDay(String weekDay){
+        this.weekDay=weekDay;
+    }
+
+    public void getSubjHour(){
+        this.subjHour=subjHour;
+    }
+
+
 }
--- src/GroupProject1_Judith_Annina_Christian/Teacher.java
+++ src/GroupProject1_Judith_Annina_Christian/Teacher.java
@@ -1,4 +1,28 @@
 package GroupProject1_Judith_Annina_Christian;
 
+import java.util.ArrayList;
+
 public class Teacher {
+    private String teacherName;
+    private ArrayList<Subject>subject;
+
+    public Teacher(String teacherName, ArrayList<Subject>subject){
+        this.teacherName= teacherName;
+        this.subject = subject;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+    public void getTeacherName(){
+        this.teacherName=teacherName;
+    }
+
+    public void setSubject(ArrayList<Subject> subject) {
+        this.subject = subject;
+    }
+
+    public void getSubject(){
+        this.subject = subject;
+    }
 }
