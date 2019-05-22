888f2c4449f8feee607891dbdf9ad3fd4f523dc0
judith <judithmarialang@gmail.com>
2019-05-22 10:11:49 +0200

    add subject fields in class subject

--- src/GroupProject1_Judith_Annina_Christian/Subject.java
+++ src/GroupProject1_Judith_Annina_Christian/Subject.java
@@ -1,4 +1,33 @@
 package GroupProject1_Judith_Annina_Christian;
 
+import java.util.ArrayList;
+
 public class Subject {
+
+    private String subjName;
+    private ArrayList<Teacher>teacher;
+
+    public Subject (String subjName, ArrayList<Teacher>teacher){
+        this.subjName= subjName;
+        this.teacher = teacher;
+
+    }
+
+    public void setSubjName(String subjName) {
+        this.subjName = subjName;
+    }
+
+    public void getSubjName(){
+        this.subjName = subjName;
+    }
+
+    public void getTeacher(){
+        this.teacher = teacher;
+    }
+
+    public void setTeacher(ArrayList<Teacher>teacher){
+        this.teacher=teacher;
+    }
 }
+
+
