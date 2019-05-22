0e80354b60f704c06b0a778eb636266571eabc72
judith <judithmarialang@gmail.com>
2019-05-22 13:54:46 +0200

    adds subjects to teachers

--- src/GroupProject1_Judith_Annina_Christian/Main.java
+++ src/GroupProject1_Judith_Annina_Christian/Main.java
@@ -8,19 +8,19 @@ public class Main {
     public static void main(String[] args) {
 
         Teacher t1 = new Teacher("Mr. Dark", new String[]{"Math", "Biology", "Physics"});
-        Teacher t2 = new Teacher("Mrs. Golightly",);
-        Teacher t3 = new Teacher("Mr. Just",);
-        Teacher t4 = new Teacher("Mr. Happy", );
-        Teacher t5 = new Teacher("Mr. Gloriuous", );
-        Teacher t6 = new Teacher("Mr. Brilliant", );
-        Teacher t7 = new Teacher("Mrs. Absolute", );
-        Teacher t8 = new Teacher("Mrs. Lord", );
-        Teacher t9 = new Teacher("Mrs. King", );
-        Teacher t10 = new Teacher("Mrs. Allknows", );
-        Teacher t11 = new Teacher("Mrs. Onpoint", );
-        Teacher t12 = new Teacher("Mrs. Strong", );
-        Teacher t13 = new Teacher("Mr. Nothappy", );
-        Teacher t14 = new Teacher("Mr. Sure", );
+        Teacher t2 = new Teacher("Mrs. Golightly",new String[]{"English", "Art", "Geography"});
+        Teacher t3 = new Teacher("Mr. Just",new String[]{"PhyEdu", "Math", "Physics"});
+        Teacher t4 = new Teacher("Mr. Happy", new String[]{"PhyEdu", "Geography", "English"});
+        Teacher t5 = new Teacher("Mr. Gloriuous", new String[]{"Art", "Biology", "Geography"});
+        Teacher t6 = new Teacher("Mr. Brilliant", new String[]{"English", "Math", "Art"});
+        Teacher t7 = new Teacher("Mrs. Absolute", new String[]{"PhyEdu", "Georgraphy", "Math"});
+        Teacher t8 = new Teacher("Mrs. Lord", new String[]{"Art", "PhyEdu", "Physics"});
+        Teacher t9 = new Teacher("Mrs. King", new String[]{"English", "Biology", "Physics"});
+        Teacher t10 = new Teacher("Mrs. Allknows",new String[]{"PhyEdu", "Geography", "English"} );
+        Teacher t11 = new Teacher("Mrs. Onpoint", new String[]{"Biology", "Georgraphy", "Art"});
+        Teacher t12 = new Teacher("Mrs. Strong", new String[]{"Physics", "PhyEdu", "Math"});
+        Teacher t13 = new Teacher("Mr. Nothappy", new String[]{"Biology", "Math", "Physics"});
+        Teacher t14 = new Teacher("Mr. Sure", new String[]{"Art", "Biology", "Math"});
 
         String[] subjects = {"Math", "English", "Biology", "Geography", "Physics", "Art", "PhyEdu"};
 
