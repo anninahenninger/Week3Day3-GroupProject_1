bc729c26f5f810f21a73f62164a8aaeee349e4aa
Annina Henninger <annina.henninger@aon.at>
2019-05-22 12:45:53 +0200

    removing of 2 classes

--- src/GroupProject1_Judith_Annina_Christian/Schedule.java
+++ src/GroupProject1_Judith_Annina_Christian/Schedule.java
@@ -3,32 +3,13 @@ package GroupProject1_Judith_Annina_Christian;
 import java.util.ArrayList;
 
 public class Schedule {
+    private String[][][] schedule;
 
-
-    private String weekDay;
-    private String [][] subjHour; //subject/hour
-
-
-
-    public Schedule(String weekDay, String [][] subjHour){
-
-        this.weekDay = weekDay;
-        this.subjHour= subjHour;
-
-
+    public Schedule(String[][][] schedule){
+        this.schedule= schedule;
     }
 
-    public void getWeekDay(){
-        this.weekDay = weekDay;
+    public Teacher teacherAbsent(Teacher teacher){
+
     }
-
-    public void setWeekDay(String weekDay){
-        this.weekDay=weekDay;
-    }
-
-    public void getSubjHour(){
-        this.subjHour=subjHour;
-    }
-
-
 }
