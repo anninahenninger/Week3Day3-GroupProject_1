89841b54ada81f40ba62119c8f242152eb1bb04d
Annina Henninger <annina.henninger@aon.at>
2019-05-22 10:23:52 +0200

    adds fields and constructor of class Student

--- src/GroupProject1_Judith_Annina_Christian/Student.java
+++ src/GroupProject1_Judith_Annina_Christian/Student.java
@@ -1,4 +1,32 @@
 package GroupProject1_Judith_Annina_Christian;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Student {
+    private Classroom classroom;
+    private ArrayList<Subject> subjects;
+    private HashMap<Subject, String> grades;
+    private String email;
+    private String name;
+    private String mommy;
+    private String daddy;
+    private int age;
+    private String avgGrade;
+
+    public Student(String name, int age, Classroom classroom, ArrayList<Subject> subjects, String email, String mommy,
+                   String daddy){
+        this.name = name;
+        this.age = age;
+        this.classroom = classroom;
+        this.subjects = subjects;
+        this.email = email;
+        this.mommy = mommy;
+        this.daddy = daddy;
+        this.grades = setGrade();
+        this.avgGrade = setAvgGrade();
+    }
+    
+    }
 }
+
